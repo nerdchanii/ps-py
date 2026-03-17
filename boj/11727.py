ab6a415d1f3f79c6b8198c94a67248c87d879f84
nerdchanii <nerdchanii@gmail.com>
2026-03-17 16:00:53 +0900

    solve 2xn tile

---
+++ boj/11727.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+dp = [0] * (n + 1)
+
+
+dp[0] = 0
+dp[1] = 1
+dp[2] = 3
+for i in range(2, n + 1):
+    dp[i] = dp[i - 1] + (2 * dp[i - 2])
+
+print(dp[n])
