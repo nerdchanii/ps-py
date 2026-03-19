1bf54e07b5716664581ac3253462ff92dadf4b6f
nerdchanii <nerdchanii@gmail.com>
2026-03-19 10:25:27 +0900

    feedback: bfs feedback

--- boj/16928.py
+++ boj/16928.py
@@ -1,40 +1,50 @@
-# 접근법
+# [접근법]
 #
 #  X = Y 맵을 만들어두고, 움직일수 있는 6개의 주사위를 집어넣고, hook 패턴으로 해당 칸이면 수정된 칸을 리턴,
 #  BFS를 이용하면 될 것.
 #
-
+# [Feedback]
+# - 처음에 getattr(hooks, key, default)이렇게 썼으나, 이건 dict의 key조회방법이 아님
+#   - getattr은 객체용
+#   - hooks.get(key, default)로 쓰면 더 깔끔
+# - visited[1] 도 True로 해주는게 다른 BFS풀떄 더 안전
+# 
 
 import sys
 from collections import deque
 
-dices = [1, 2, 3, 4, 5, 6]
-visited = [False] * 101
-hooks = {}
 END = 100
+visited = [False] * 101
+hooks: dict[int, int] = {}
 
 
-n, m = map(int, sys.stdin.readline().split(" "))
+n, m = map(int, sys.stdin.readline().split())
 
 for _ in range(n + m):
-    k, v = map(int, sys.stdin.readline().split(" "))
+    k, v = map(int, sys.stdin.readline().split())
     hooks[k] = v
 
 
-def solve(dices, hooks):
-    q = deque()  # pos, step
-    q.append((1, 0))
-    while len(q):
+def solve():
+    q = deque([(1, 0)])  # pos, step
+    visited[1] = True
+
+    while q:
         pos, step = q.popleft()
+
         if pos == END:
             return step
-        for dice in dices:
-            next = pos + dice
-            if next in hooks:
-                next = hooks[next]
-            if next <= 100 and not visited[next]:
-                q.append((next, step + 1))
-                visited[next] = True
+
+        for dice in range(1, 7):
+            nxt = pos + dice
+            if 100 < nxt:
+                continue
+
+            nxt: int = hooks.get(nxt, nxt)
+
+            if not visited[nxt]:
+                q.append((nxt, step + 1))
+                visited[nxt] = True
 
 
-print(solve(dices, hooks))
+print(solve())
